09636ad974238dbf239a016cc385a4b7521593f9
Ming Ho <mrcow138@gmail.com>
2016-08-29 23:56:40 +0800

    123

--- ionicApp/basic/www/js/controllers.js
+++ ionicApp/basic/www/js/controllers.js
@@ -21,8 +21,38 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $ionicActionSheet, $timeout) {
   $scope.settings = {
     enableFriends: true
   };
+  $scope.show = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your album',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+ };
+
+  // Alert after action
+  $scope.hello = function(section) {
+    alert('You just swiped ' + section + '!');
+  }
+
 });
